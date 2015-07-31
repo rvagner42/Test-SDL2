052a928784eeaabb36cd7f99cca5540925379ae5
rvagner42 <rvagner@student.42.fr>
2015-07-31 11:13:55 +0200

    modif sprite animation

--- Sources/SpriteChar.cpp
+++ Sources/SpriteChar.cpp
@@ -5,8 +5,8 @@ SpriteChar::SpriteChar(SDL_Renderer *rend, std::string const &name, int x, int y
 	SDL_QueryTexture(this->_texture, NULL, NULL, &(this->_width), &(this->_height));
 	this->_crop.x = 0;
 	this->_crop.y = 0;
-	this->_crop.w = this->_width / 12;
-	this->_crop.h = this->_height;
+	this->_crop.w = this->_width / 7;
+	this->_crop.h = this->_height / 4;
 	this->_frame = 0;
 	this->_dir = 0;
 }
@@ -16,8 +16,8 @@ SpriteChar::SpriteChar(SDL_Renderer *rend, std::string const &name, int w, int h
 	SDL_QueryTexture(this->_texture, NULL, NULL, &(this->_width), &(this->_height));
 	this->_crop.x = 0;
 	this->_crop.y = 0;
-	this->_crop.w = this->_width / 12;
-	this->_crop.h = this->_height;
+	this->_crop.w = this->_width / 7;
+	this->_crop.h = this->_height / 4;
 	this->_frame = 0;
 	this->_dir = 0;
 }
@@ -36,7 +36,7 @@ void			SpriteChar::animate(int dirX, int dirY)
 {
 	if (dirX == 0 && dirY == 0)
 	{
-		this->_crop.x = this->_dir * (this->_width / 4);
+		this->_crop.x = 0;
 		this->_frame = 0;
 	}
 	else
@@ -48,12 +48,22 @@ void			SpriteChar::animate(int dirX, int dirY)
 			this->_dir = 1;
 		if (dirX == -1)
 			this->_dir = 3;
-		(this->_frame == 32) ? this->_frame = 0 : this->_frame++;
-		this->_crop.x = this->_dir * (this->_width / 4);
-		if (this->_frame < 8)
-			this->_crop.x += this->_width / 12;
-		else if (this->_frame > 15 && this->_frame < 24)
-			this->_crop.x += this->_width / 6;
+		(this->_frame == 42) ? this->_frame = 0 : this->_frame++;
+		if (this->_frame < 6)
+			this->_crop.x = this->_crop.w;
+		else if (this->_frame >= 6 && this->_frame < 12)
+			this->_crop.x = 2 * this->_crop.w;
+		else if (this->_frame >= 12 && this->_frame < 18)
+			this->_crop.x = 3 * this->_crop.w;
+		else if (this->_frame >= 18 && this->_frame < 24)
+			this->_crop.x = 4 * this->_crop.w;
+		else if (this->_frame >= 24 && this->_frame < 30)
+			this->_crop.x = 5 * this->_crop.w;
+		else if (this->_frame >= 30 && this->_frame < 36)
+			this->_crop.x = 6 * this->_crop.w;
+		else if (this->_frame >= 36 && this->_frame < 42)
+			this->_crop.x = 0;
 	}
+	this->_crop.y = this->_dir * this->_crop.h;
 	this->draw();
 }
