026e9c581df15b6223d4449f78b712d97dd53f89
rvagner42 <rvagner@student.42.fr>
2015-08-07 16:17:38 +0200

    ok

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,20 +1,6 @@
 #include "rpg.h"
-
 #include "Game.hpp"
-/*
-   void            pixel( SDL_Surface *surface, Vertex const &vertex )
-   {
-   Uint32 *data = (Uint32 *)surface->pixels;
 
-   Uint32 color = vertex.getColor().getRed() * 256 * 256 + vertex.getColor().getGreen() * 256 + vertex.getColor().getBlue();
-
-   if (vertex.getY() < 0 || vertex.getX() < 0 || vertex.getY() > H || vertex.getX() > W)
-   {
-   throw(std::exception());
-   }
-   data[ ( static_cast<int>(vertex.getY()) * surface->w ) + static_cast<int>(vertex.getX()) ] = color;
-   }
- */
 int		main(void)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) >= 0)
